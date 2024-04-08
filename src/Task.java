100ea182e5f06a8b05678a5163daa8448b616312
Ne-protiv <andreibelocopytov20134@gmail.com>
2024-04-09 01:57:21 +0300

    result

---
+++ src/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    public static void main(String[] args) {
+        boolean varboolean = false;
+        byte varbite = 127;
+        short varshort = 999;
+        int varint = 123;
+        long varlong = 1124;
+        double vardouble = 12;
+        float varfloat = 19.22f;
+    }
+}
